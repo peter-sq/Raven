3b34d25eca14f61e034ac22e1dc4bf54ca4f538e
peter-sq <awe.petersq@gmail.com>
2025-01-29 02:54:28 +0100

    install knex.js

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ export default {
   development: {
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'Brown@55',
-      database: 'raven',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       directory: './db/migration',
